8414dda7a5865bf61b8e955558453a733f9077d2
Christopher <christophermakombe.developer@gmail.com>
2023-05-12 21:41:36 +0200

    hey

---
+++ pages/Success.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const success = () => {
+  return (
+<div style={{ display: "flex", justifyContent: "center", alignItems: "center",marginTop:"90px" }}>
+        <div id="load">
+            <div>W</div>
+            <div>E</div>
+            <div>L</div>
+            <div>C</div>
+            <div>O</div>
+            <div>M</div>
+            <div>E</div>
+        </div>
+</div>
+  )
+}
+
+export default success;
